0a2737dc265c9ae8a56a0f23cb8cb4dedbea4e35
AbdoOMohammed <124630839+AbdoOMohammed@users.noreply.github.com>
2023-04-15 12:18:37 +0200

    Add files via upload

---
+++ JSproject.js
@@ -0,0 +1,41 @@
+var disSecsion_1 = document.getElementById("secsion_1");
+var disSecsion_2 = document.getElementById("secsion_2");
+var disSecsion_3 = document.getElementById("secsion_3");
+var disSecsion_4 = document.getElementById("secsion_4");
+var disSecsion_5 = document.getElementById("secsion_5");
+
+function funSecsion_1(){
+    disSecsion_1.style.display="block";
+    disSecsion_2.style.display="none";
+    disSecsion_3.style.display="none";
+    disSecsion_4.style.display="none";
+    disSecsion_5.style.display="none";
+}
+function funSecsion_2(){
+    disSecsion_2.style.display="block";
+    disSecsion_1.style.display="none";
+    disSecsion_3.style.display="none";
+    disSecsion_4.style.display="none";
+    disSecsion_5.style.display="none";
+}
+function funSecsion_3(){
+    disSecsion_3.style.display="block";
+    disSecsion_1.style.display="none";
+    disSecsion_2.style.display="none";
+    disSecsion_4.style.display="none";
+    disSecsion_5.style.display="none";
+}
+function funSecsion_4(){
+    disSecsion_4.style.display="block";
+    disSecsion_2.style.display="none";
+    disSecsion_3.style.display="none";
+    disSecsion_1.style.display="none";
+    disSecsion_5.style.display="none";
+}
+function funSecsion_5(){
+    disSecsion_5.style.display="block";
+    disSecsion_2.style.display="none";
+    disSecsion_3.style.display="none";
+    disSecsion_4.style.display="none";
+    disSecsion_1.style.display="none";
+}
