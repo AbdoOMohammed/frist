6a1ad39e111fd56ae724dac1153f53a38b2a5001
AbdoOMohammed <124630839+AbdoOMohammed@users.noreply.github.com>
2023-04-16 09:17:23 +0200

    Update jqueryProject.js

--- jqueryProject.js
+++ jqueryProject.js
@@ -1,12 +1,27 @@
+
 var over = document.getElementById("boody");
 
 $("#openMenu").click(function(){
     over.style.overflowY="hidden";
-    $("#menu").show(300);
-    $(".background").show(100);
+    $("#menu").show(400);
+    $(".background").show(300);
 })
+
 $("#close").click(function(){
     over.style.overflowY="scroll";
     $("#menu").hide(400);
-    $(".background").hide(100);
+    $(".background").hide(300);
 })
+
+$(".end").click(function(){
+    over.style.overflowY="hidden";
+    $("#menu").hide(600);
+    $(".background").hide(600);
+})
+///////////////////////////////
+
+$(".b3").click(function(){
+    $(".hhh").animate({
+        width: "toggle"
+    });
+});
